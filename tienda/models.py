c92e24a233a421a8cd5809b8a4b035d05ab2ddcd
TuGatita777 <maxi.gonzalezg@duocuc.cl>
2023-05-22 11:38:24 -0400

    Arreglos y bd

--- tienda/models.py
+++ tienda/models.py
@@ -10,33 +10,34 @@ class Usuario (models.Model):
     emailu = models.CharField(max_length=20)
     nombre_usuariou = models.CharField(max_length=15)
     contrasenau = models.CharField(max_length=15)
-    nombreu = models.CharField(blank,max_length=30)
-    imagenu
-    rol = models.Foreignkey(Rol,on_delete=models.CASCADE)
-
-class Compra (models.Model):
-    id_comprac = models.AutoField(primary_key=True)
-    fechac = models.DataField(verbose_name='Fecha de compra')
-    rutc = models.CharField(max_length=16)
-    totalc = models.integerField(verbose_name='Total De la Compra')
-    id_usuario = models.Foreignkey(Usuario,on_delete=models.CASCADE)
-    id_detalles = models.Foreignkey(Detalles_compra)
-
-class Detalles_compra (models.Model):
-    id_detallesc = models.AutoField(primary_key=True)
-    subtotal = models.integerField (verbose_name='Subtotal de la compra')
-    cantidad = models.integerField(verbose_name='cantidad de productos')
-    id_juego = models.Foreignkey(Videojuegos,on_delete=models.CASCADE)
-
-class Videojuegos (models.Model):
-    id_juego = models.AutoField(primary_key=True)
-    nombrev = models.CharField(max_length=30)
-    descripcion = models.CharField(max_length=300)
-    precio = models.integerField(verbose_name='Precio de videojuegos')
-    imagenv
-    seccion = models.Foreignkey(Seccion,on_delete=models.CASCADE)
+    nombreu = models.CharField(blank=True,max_length=30)
+    imagenu = models.ImageField(blank=True,verbose_name="Imagen De Usuario",upload_to="ImagenUser")
+    rol = models.ForeignKey(Rol,on_delete=models.CASCADE)
 
 
 class Seccion (models.Model):
         id_seccions = models.AutoField(primary_key=True)
         nombres = models.CharField(max_length=30)
+
+class Videojuegos (models.Model):
+    id_juego = models.AutoField(primary_key=True)
+    nombrev = models.CharField(max_length=30)
+    descripcion = models.CharField(max_length=300)
+    precio = models.FloatField(verbose_name='Precio de videojuegos')
+    imagenv = models.ImageField(blank=True,verbose_name="Imagen de videojuegos", upload_to="ImagenVideojuegos")
+    seccion = models.ForeignKey(Seccion, on_delete=models.CASCADE)
+
+
+class Detallesc (models.Model):
+    id_detallesc = models.AutoField(primary_key=True)
+    subtotal = models.FloatField (verbose_name='Subtotal de la compra')
+    cantidad = models.IntegerField(verbose_name='cantidad de productos')
+    id_juego = models.ForeignKey(Videojuegos, on_delete=models.CASCADE)
+
+class Compra (models.Model):
+    id_comprac = models.AutoField(primary_key=True)
+    fechac = models.DateField(verbose_name='Fecha de compra')
+    rutc = models.CharField(max_length=16)
+    totalc = models.IntegerField(verbose_name='Total De la Compra')
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    id_detalles = models.ForeignKey(Detallesc, on_delete=models.CASCADE)
--- tienda/urls.py
+++ tienda/urls.py
@@ -15,37 +15,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views 
-import tienda,registro,olvide_contrasena,Nosotros,inicio_sesion,editarPerfil,Cuenta,Colaboracion,Cambiar_Contrasena,
- Administracion,agregar,AgregarN,AgregarP,AgregarPC,AgregarX,Eliminar,Modificar
- seccion_playstation,bloodborne,
- seccion_nintendo,zelda,
- seccion_Xbox,haloi,
- Seccion_Pc,cyber,
+from .views import tienda,registro,olvide_contrasena,Nosotros,inicio_sesion,editarPerfil,Cuenta,Colaboracion,Cambiar_Contrasena,Administracion,Agregar,AgregarN,AgregarP,AgregarPC,AgregarX,Eliminar,Modificar,seccion_playstation,bloodborne,seccion_nintendo,zelda,seccion_Xbox,haloi,seccion_Pc,cyber,carrito
  
 
 
 urlpatterns = [
     #Paginas Principales
     path('',tienda,name="tienda"),
-    path('registro/',registro,name="registro")
-    path('olvide_contrasena/',olvide_contrasena,name="olvide_contrasena")
-    path('Nosotros/',Nosotros,name="Nosotros")
-    path('inicio_sesion/',inicio_sesion,name="inicio-sesion")
-    path('editar_Perfil/',editarPerfil,name="editarPerfil")
-    path('Cuenta/',Cuenta,name="Cuenta")
-    path('Colaboracion/',Colaboracion,name="Colaboracion")
-    path('Cambiar_Contrasena/',Cambiar_Contrasena,name="Cambiar_Contrasena")
+    path('registro/',registro,name="registro"),
+    path('olvide_contrasena/',olvide_contrasena,name="olvide_contrasena"),
+    path('Nosotros/',Nosotros,name="Nosotros"),
+    path('inicio_sesion/',inicio_sesion,name="inicio_sesion"),
+    path('editar_Perfil/',editarPerfil,name="editarPerfil"),
+    path('Cuenta/',Cuenta,name="Cuenta"),
+    path('Colaboracion/',Colaboracion,name="Colaboracion"),
+    path('Cambiar_Contrasena/',Cambiar_Contrasena,name="Cambiar_Contrasena"),
     
     #Admin
-    path('Administracion/',Administracion,name="Administracion")
-    path('Agregar/',Agregar,name="Agregar")
-    path('Agregar/Nintendo/',AgregarN,name="AgregarN")
-    path('Agregar/Xbox/',AgregarX,name="AgregarX")
-    path('Agregar/Playstation/',AgregarP,name="AgregarP")
-    path('Agregar/Steam',AgregarPC,name="AgregarPC")
-    path('Eliminar/',Eliminar,name="Eliminar")
-    path('Modificar/',Modificar,name="Modificar")
+    path('Administracion/',Administracion,name="Administracion"),
+    path('Agregar/',Agregar,name="Agregar"),
+    path('Agregar/Nintendo/',AgregarN,name="AgregarN"),
+    path('Agregar/Xbox/',AgregarX,name="AgregarX"),
+    path('Agregar/Playstation/',AgregarP,name="AgregarP"),
+    path('Agregar/Steam',AgregarPC,name="AgregarPC"),
+    path('Eliminar/',Eliminar,name="Eliminar"),
+    path('Modificar/',Modificar,name="Modificar"),
 
 
     #seccion play
@@ -53,18 +47,18 @@ urlpatterns = [
     path('playstation/bloodborne/',bloodborne,name="bloodborne"),
 
     #Seccion Xbox
-    path('Xbox/',seccion_Xbox,name="seccion_Xbox")
-    path('Xbox/haloInfinite/',haloi,name="haloi")
+    path('Xbox/',seccion_Xbox,name="seccion_Xbox"),
+    path('Xbox/haloInfinite/',haloi,name="haloi"),
 
     #seccion nintendo 
     path('nintendo/',seccion_nintendo,name="seccion_nintendo"),
     path('nintendo/zelda/',zelda,name="zelda"),
 
     #Seccion Pc
-    path('Steam/',Seccion_Pc,name="Seccion_Pc")
-    path('Steam/Cyberpunk/',cyber,name="cyber")
+    path('Steam/',seccion_Pc,name="Seccion_Pc"),
+    path('Steam/Cyberpunk/',cyber,name="cyber"),
 
     #Carrito
-    path('Carrito/',Carrito,name="Carrito")
+    path('Carrito/',carrito,name="Carrito"),
 
 ]
