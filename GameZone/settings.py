7d15c8189306b25dfca028c61e3cb812a041429b
TuGatita777 <maxi.gonzalezg@duocuc.cl>
2023-05-26 22:53:15 -0400

    cambios en plantilla y settings

--- GameZone/settings.py
+++ GameZone/settings.py
@@ -77,13 +77,13 @@ WSGI_APPLICATION = 'GameZone.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
-        'NAME': '127.0.0.1:1521/orcl', #20 o 21 es xe 
+        'NAME': '127.0.0.1:1521/orcl',
         'USER': 'gamezone',
         'PASSWORD': 'zonajuegos',
         'TEST':{
-          'USER': 'default_test',
-          'TBLSPACE': 'default_test_tbls',
-          'TBLSPACE_TMP:': 'default_test_tbls_tmp',  
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
         },
     }
 }
